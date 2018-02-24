5aa84162b4bb19a2e124675b6f7290e57a525974
Davanon <davanon2014@gmail.com>
2018-02-24 12:00:07 +0530

    First

---
+++ src/main/groovy/com/iqmsoft/groovy/SomeController.groovy
@@ -0,0 +1,12 @@
+package com.iqmsoft.groovy
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+
+class SomeController {
+    @RequestMapping("/hello.htm")
+    public ModelAndView helloWorld() {
+        print "hello guys ..."
+        return new ModelAndView("hello")
+    }
+}
